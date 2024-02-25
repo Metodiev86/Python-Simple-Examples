4b39bdb2e54556c068fc99329c3a6e8828cbb854
Stoyan Metodiev <161215508+Metodiev86@users.noreply.github.com>
2024-02-25 15:22:56 +0200

    Add files via upload

--- Dice_Roller_Example.py
+++ Dice_Roller_Example.py
@@ -41,7 +41,7 @@ dise = []
 total = 0
 num_of_dice = int(input("Колко зара използваме: "))
 
-for die in range(num_of_dice):
+for die in range(num_of_dice): #Произволно число, показващ зара
   dise.append(random.randint(1,6))
 print(dise)
 
@@ -49,12 +49,12 @@ print(dise)
 #   for line in dice_art.get(dise[die]):
 #     print(line, end="")
 
-for line in range(5):
+for line in range(5): #Отпечатване на графиките на заровете
   for die in dise:
     print(dice_art.get(die)[line], end="")
   print()
 
 
-for die in dise:
+for die in dise: #Сума от избраните зарове
   total += die
 print (total)
